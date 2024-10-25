1653d06fc49f314ca71df18a7f806228994458d1
Finn Williams <finnreg47@gmail.com>
2024-10-24 23:32:32 -0400

    > use macro endpoint definitions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{delete, get, patch, post, web, App, HttpResponse, HttpServer, Responder};
 
+#[get("/ping")]
 pub async fn ping() -> impl Responder {
     HttpResponse::Ok().body("pong")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/ping", web::get().to(ping)))
+    HttpServer::new(|| App::new())
         .bind("127.0.0.1:8080")? // Desired IP and port
         .run()
         .await
